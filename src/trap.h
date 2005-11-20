69e920f57bd8c0ba7a26eac991a538b8ee39f4ee
FUNABARA Masao <masao@masoo.jp>
2005-11-20 06:47:14 +0000

    trap.c : 関数の外部宣言のチェック

--- src/trap.h
+++ src/trap.h
@@ -1,11 +1,11 @@
 #ifndef __TRAP_H__
 #define __TRAP_H__
 
-extern void add_traps(void);
-extern void search(short n, boolean is_auto);
-extern void id_trap(void);
-extern void show_traps(void);
 extern int trap_at(register int row, register int col);
 extern void trap_player(short row, short col);
+extern void add_traps(void);
+extern void id_trap(void);
+extern void show_traps(void);
+extern void search(short n, boolean is_auto);
 
 #endif /* __TRAP_H__ */
