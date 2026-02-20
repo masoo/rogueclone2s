9221e2f3fab30fb3bc42208fd840b20fd110a127
FUNABARA Masao <masao@masoo.jp>
2026-02-20 12:26:01 +0900

    セーブ機能の改善とリファクタリングを実施し、データの読み書きをリトルエンディアン形式で行う関数を追加

--- src/save.c
+++ src/save.c
@@ -12,6 +12,7 @@
 
 #include <curses.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,6 +30,9 @@
 #include "rogue.h"
 #include "save.h"
 #include "score.h"
+
+#define SAVE_MAGIC "RC2S"
+#define SAVE_VERSION 1
 
 short write_failed = 0;
 char *save_file = "";
@@ -55,6 +59,264 @@ extern bool score_only;
 extern short m_moves;
 
 extern bool msg_cleared;
+
+/*
+ * r_read
+ * ファイルからバッファに読み込む
+ */
+void
+r_read(FILE *fp, char *buf, int n)
+{
+	if (fread(buf, sizeof(char), n, fp) != n) {
+		clean_up("ファイルが読めません。");
+	}
+}
+
+/*
+ * r_write
+ * バッファの内容をファイルに書き込む
+ */
+void
+r_write(FILE *fp, char *buf, int n)
+{
+	if (!write_failed) {
+		if (fwrite(buf, sizeof(char), n, fp) != n) {
+			message("ファイルに書けません。", 0);
+			sound_bell();
+			write_failed = 1;
+		}
+	}
+}
+
+/* リトルエンディアンでの型付き書き込み */
+
+static void
+write_bool(FILE *fp, bool val)
+{
+	uint8_t v = val ? 1 : 0;
+	r_write(fp, (char *)&v, 1);
+}
+
+static void
+write_int8(FILE *fp, int8_t val)
+{
+	r_write(fp, (char *)&val, 1);
+}
+
+static void
+write_uint8(FILE *fp, uint8_t val)
+{
+	r_write(fp, (char *)&val, 1);
+}
+
+static void
+write_int16(FILE *fp, int16_t val)
+{
+	uint8_t buf[2];
+	uint16_t u;
+	memcpy(&u, &val, 2);
+	buf[0] = u & 0xFF;
+	buf[1] = (u >> 8) & 0xFF;
+	r_write(fp, (char *)buf, 2);
+}
+
+static void
+write_uint16(FILE *fp, uint16_t val)
+{
+	uint8_t buf[2];
+	buf[0] = val & 0xFF;
+	buf[1] = (val >> 8) & 0xFF;
+	r_write(fp, (char *)buf, 2);
+}
+
+static void
+write_int32(FILE *fp, int32_t val)
+{
+	uint8_t buf[4];
+	uint32_t u;
+	memcpy(&u, &val, 4);
+	buf[0] = u & 0xFF;
+	buf[1] = (u >> 8) & 0xFF;
+	buf[2] = (u >> 16) & 0xFF;
+	buf[3] = (u >> 24) & 0xFF;
+	r_write(fp, (char *)buf, 4);
+}
+
+static void
+write_uint32(FILE *fp, uint32_t val)
+{
+	uint8_t buf[4];
+	buf[0] = val & 0xFF;
+	buf[1] = (val >> 8) & 0xFF;
+	buf[2] = (val >> 16) & 0xFF;
+	buf[3] = (val >> 24) & 0xFF;
+	r_write(fp, (char *)buf, 4);
+}
+
+/* リトルエンディアンでの型付き読み込み */
+
+static bool
+read_bool(FILE *fp)
+{
+	uint8_t v;
+	r_read(fp, (char *)&v, 1);
+	return v != 0;
+}
+
+static int8_t
+read_int8(FILE *fp)
+{
+	int8_t val;
+	r_read(fp, (char *)&val, 1);
+	return val;
+}
+
+static uint8_t
+read_uint8(FILE *fp)
+{
+	uint8_t val;
+	r_read(fp, (char *)&val, 1);
+	return val;
+}
+
+static int16_t
+read_int16(FILE *fp)
+{
+	uint8_t buf[2];
+	r_read(fp, (char *)buf, 2);
+	uint16_t u = (uint16_t)buf[0] | ((uint16_t)buf[1] << 8);
+	int16_t val;
+	memcpy(&val, &u, 2);
+	return val;
+}
+
+static uint16_t
+read_uint16(FILE *fp)
+{
+	uint8_t buf[2];
+	r_read(fp, (char *)buf, 2);
+	return (uint16_t)buf[0] | ((uint16_t)buf[1] << 8);
+}
+
+static int32_t
+read_int32(FILE *fp)
+{
+	uint8_t buf[4];
+	r_read(fp, (char *)buf, 4);
+	uint32_t u = (uint32_t)buf[0] | ((uint32_t)buf[1] << 8) |
+	    ((uint32_t)buf[2] << 16) | ((uint32_t)buf[3] << 24);
+	int32_t val;
+	memcpy(&val, &u, 4);
+	return val;
+}
+
+static uint32_t
+read_uint32(FILE *fp)
+{
+	uint8_t buf[4];
+	r_read(fp, (char *)buf, 4);
+	return (uint32_t)buf[0] | ((uint32_t)buf[1] << 8) |
+	    ((uint32_t)buf[2] << 16) | ((uint32_t)buf[3] << 24);
+}
+
+/* 構造体のフィールド別シリアライズ */
+
+static void
+write_fighter(FILE *fp, fighter *f)
+{
+	write_int16(fp, f->hp_current);
+	write_int16(fp, f->hp_max);
+	write_int16(fp, f->str_current);
+	write_int16(fp, f->str_max);
+	write_int32(fp, (int32_t)f->gold);
+	write_int16(fp, f->exp);
+	write_int32(fp, (int32_t)f->exp_points);
+	write_int16(fp, f->row);
+	write_int16(fp, f->col);
+	write_int16(fp, f->fchar);
+	write_int16(fp, f->moves_left);
+}
+
+static void
+read_fighter(FILE *fp, fighter *f)
+{
+	f->armor = NULL;
+	f->weapon = NULL;
+	f->left_ring = NULL;
+	f->right_ring = NULL;
+	f->hp_current = read_int16(fp);
+	f->hp_max = read_int16(fp);
+	f->str_current = read_int16(fp);
+	f->str_max = read_int16(fp);
+	f->pack.next_object = NULL;
+	f->gold = read_int32(fp);
+	f->exp = read_int16(fp);
+	f->exp_points = read_int32(fp);
+	f->row = read_int16(fp);
+	f->col = read_int16(fp);
+	f->fchar = read_int16(fp);
+	f->moves_left = read_int16(fp);
+}
+
+static void
+write_trap(FILE *fp, trap *t)
+{
+	write_int16(fp, t->trap_type);
+	write_int16(fp, t->trap_row);
+	write_int16(fp, t->trap_col);
+}
+
+static void
+read_trap(FILE *fp, trap *t)
+{
+	t->trap_type = read_int16(fp);
+	t->trap_row = read_int16(fp);
+	t->trap_col = read_int16(fp);
+}
+
+static void
+write_door(FILE *fp, door *d)
+{
+	write_int16(fp, d->oth_room);
+	write_int16(fp, d->oth_row);
+	write_int16(fp, d->oth_col);
+	write_int16(fp, d->door_row);
+	write_int16(fp, d->door_col);
+}
+
+static void
+read_door(FILE *fp, door *d)
+{
+	d->oth_room = read_int16(fp);
+	d->oth_row = read_int16(fp);
+	d->oth_col = read_int16(fp);
+	d->door_row = read_int16(fp);
+	d->door_col = read_int16(fp);
+}
+
+static void
+write_room(FILE *fp, room *r)
+{
+	write_int8(fp, r->bottom_row);
+	write_int8(fp, r->right_col);
+	write_int8(fp, r->left_col);
+	write_int8(fp, r->top_row);
+	for (int k = 0; k < 4; k++)
+		write_door(fp, &r->doors[k]);
+	write_uint16(fp, r->is_room);
+}
+
+static void
+read_room(FILE *fp, room *r)
+{
+	r->bottom_row = read_int8(fp);
+	r->right_col = read_int8(fp);
+	r->left_col = read_int8(fp);
+	r->top_row = read_int8(fp);
+	for (int k = 0; k < 4; k++)
+		read_door(fp, &r->doors[k]);
+	r->is_room = read_uint16(fp);
+}
 
 /*
  * save_game
@@ -105,44 +367,53 @@ save_into_file(char *sfile)
 	}
 	md_ignore_signals();
 	write_failed = 0;
+	r_write(fp, SAVE_MAGIC, 4);
+	write_uint16(fp, SAVE_VERSION);
 	(void)xxx(1);
-	r_write(fp, (char *)&detect_monster, sizeof(detect_monster));
-	r_write(fp, (char *)&cur_level, sizeof(cur_level));
-	r_write(fp, (char *)&max_level, sizeof(max_level));
+	write_bool(fp, detect_monster);
+	write_int16(fp, cur_level);
+	write_int16(fp, max_level);
 	write_string(hunger_str, fp);
 	write_string(login_name, fp);
-	r_write(fp, (char *)&party_room, sizeof(party_room));
-	r_write(fp, (char *)&party_counter, sizeof(party_counter));
+	write_int16(fp, party_room);
+	write_int16(fp, party_counter);
 	write_pack(&level_monsters, fp);
 	write_pack(&level_objects, fp);
-	r_write(fp, (char *)&file_id, sizeof(file_id));
+	write_int32(fp, file_id);
 	rw_dungeon(fp, 1);
-	r_write(fp, (char *)&foods, sizeof(foods));
-	r_write(fp, (char *)&rogue, sizeof(fighter));
+	write_int16(fp, foods);
+	write_fighter(fp, &rogue);
 	write_pack(&rogue.pack, fp);
 	rw_id(id_potions, fp, POTIONS, 1);
 	rw_id(id_scrolls, fp, SCROLS, 1);
 	rw_id(id_wands, fp, WANDS, 1);
 	rw_id(id_rings, fp, RINGS, 1);
-	r_write(fp, (char *)traps, (MAX_TRAPS * sizeof(trap)));
-	r_write(fp, (char *)is_wood, (WANDS * sizeof(bool)));
-	r_write(fp, (char *)&cur_room, sizeof(cur_room));
+	for (short i = 0; i < MAX_TRAPS; i++)
+		write_trap(fp, &traps[i]);
+	for (short i = 0; i < WANDS; i++)
+		write_bool(fp, is_wood[i]);
+	write_int16(fp, cur_room);
 	rw_rooms(fp, 1);
-	r_write(fp, (char *)&being_held, sizeof(being_held));
-	r_write(fp, (char *)&bear_trap, sizeof(bear_trap));
-	r_write(fp, (char *)&halluc, sizeof(halluc));
-	r_write(fp, (char *)&blind, sizeof(blind));
-	r_write(fp, (char *)&confused, sizeof(confused));
-	r_write(fp, (char *)&levitate, sizeof(levitate));
-	r_write(fp, (char *)&haste_self, sizeof(haste_self));
-	r_write(fp, (char *)&see_invisible, sizeof(see_invisible));
-	r_write(fp, (char *)&detect_monster, sizeof(detect_monster));
-	r_write(fp, (char *)&wizard, sizeof(wizard));
-	r_write(fp, (char *)&score_only, sizeof(score_only));
-	r_write(fp, (char *)&m_moves, sizeof(m_moves));
+	write_bool(fp, being_held);
+	write_int16(fp, bear_trap);
+	write_int16(fp, halluc);
+	write_int16(fp, blind);
+	write_int16(fp, confused);
+	write_int16(fp, levitate);
+	write_int16(fp, haste_self);
+	write_bool(fp, see_invisible);
+	write_bool(fp, detect_monster);
+	write_bool(fp, wizard);
+	write_bool(fp, score_only);
+	write_int16(fp, m_moves);
 	md_gct(&rt_buf);
 	rt_buf.second += 10; /* allow for some processing time */
-	r_write(fp, (char *)&rt_buf, sizeof(rt_buf));
+	write_int16(fp, rt_buf.year);
+	write_int16(fp, rt_buf.month);
+	write_int16(fp, rt_buf.day);
+	write_int16(fp, rt_buf.hour);
+	write_int16(fp, rt_buf.minute);
+	write_int16(fp, rt_buf.second);
 	fclose(fp);
 
 	if (write_failed) {
@@ -184,10 +455,19 @@ restore(char *fname)
 	if (md_link_count(fname) > 1) {
 		clean_up("ファイルはリンクされています。");
 	}
+	char magic[4];
+	r_read(fp, magic, 4);
+	if (memcmp(magic, SAVE_MAGIC, 4) != 0) {
+		clean_up("セーブファイルの形式が不正です。");
+	}
+	uint16_t version = read_uint16(fp);
+	if (version != SAVE_VERSION) {
+		clean_up("セーブファイルのバージョンが不正です。");
+	}
 	(void)xxx(1);
-	r_read(fp, (char *)&detect_monster, sizeof(detect_monster));
-	r_read(fp, (char *)&cur_level, sizeof(cur_level));
-	r_read(fp, (char *)&max_level, sizeof(max_level));
+	detect_monster = read_bool(fp);
+	cur_level = read_int16(fp);
+	max_level = read_int16(fp);
 	read_string(hunger_str, fp);
 
 	(void)strcpy(tbuf, login_name);
@@ -196,39 +476,46 @@ restore(char *fname)
 		clean_up("セーブファイルの持ち主が違います。");
 	}
 
-	r_read(fp, (char *)&party_room, sizeof(party_room));
-	r_read(fp, (char *)&party_counter, sizeof(party_counter));
+	party_room = read_int16(fp);
+	party_counter = read_int16(fp);
 	read_pack(&level_monsters, fp, 0);
 	read_pack(&level_objects, fp, 0);
-	r_read(fp, (char *)&saved_file_id, sizeof(saved_file_id));
+	saved_file_id = read_int32(fp);
 	if (new_file_id != saved_file_id) {
 		clean_up("これは元のセーブファイルではありません。");
 	}
 	rw_dungeon(fp, 0);
-	r_read(fp, (char *)&foods, sizeof(foods));
-	r_read(fp, (char *)&rogue, sizeof(fighter));
+	foods = read_int16(fp);
+	read_fighter(fp, &rogue);
 	read_pack(&rogue.pack, fp, 1);
 	rw_id(id_potions, fp, POTIONS, 0);
 	rw_id(id_scrolls, fp, SCROLS, 0);
 	rw_id(id_wands, fp, WANDS, 0);
 	rw_id(id_rings, fp, RINGS, 0);
-	r_read(fp, (char *)traps, (MAX_TRAPS * sizeof(trap)));
-	r_read(fp, (char *)is_wood, (WANDS * sizeof(bool)));
-	r_read(fp, (char *)&cur_room, sizeof(cur_room));
+	for (short i = 0; i < MAX_TRAPS; i++)
+		read_trap(fp, &traps[i]);
+	for (short i = 0; i < WANDS; i++)
+		is_wood[i] = read_bool(fp);
+	cur_room = read_int16(fp);
 	rw_rooms(fp, 0);
-	r_read(fp, (char *)&being_held, sizeof(being_held));
-	r_read(fp, (char *)&bear_trap, sizeof(bear_trap));
-	r_read(fp, (char *)&halluc, sizeof(halluc));
-	r_read(fp, (char *)&blind, sizeof(blind));
-	r_read(fp, (char *)&confused, sizeof(confused));
-	r_read(fp, (char *)&levitate, sizeof(levitate));
-	r_read(fp, (char *)&haste_self, sizeof(haste_self));
-	r_read(fp, (char *)&see_invisible, sizeof(see_invisible));
-	r_read(fp, (char *)&detect_monster, sizeof(detect_monster));
-	r_read(fp, (char *)&wizard, sizeof(wizard));
-	r_read(fp, (char *)&score_only, sizeof(score_only));
-	r_read(fp, (char *)&m_moves, sizeof(m_moves));
-	r_read(fp, (char *)&saved_time, sizeof(saved_time));
+	being_held = read_bool(fp);
+	bear_trap = read_int16(fp);
+	halluc = read_int16(fp);
+	blind = read_int16(fp);
+	confused = read_int16(fp);
+	levitate = read_int16(fp);
+	haste_self = read_int16(fp);
+	see_invisible = read_bool(fp);
+	detect_monster = read_bool(fp);
+	wizard = read_bool(fp);
+	score_only = read_bool(fp);
+	m_moves = read_int16(fp);
+	saved_time.year = read_int16(fp);
+	saved_time.month = read_int16(fp);
+	saved_time.day = read_int16(fp);
+	saved_time.hour = read_int16(fp);
+	saved_time.minute = read_int16(fp);
+	saved_time.second = read_int16(fp);
 
 	if (fread(buf, sizeof(char), 1, fp) > 0) {
 		clear();
@@ -316,8 +603,9 @@ rw_dungeon(FILE *fp, bool rw)
 
 	if (rw) { // 書き込み時実行
 		for (i = 0; i < ROGUE_LINES; i++) {
-			r_write(fp, (char *)dungeon[i],
-			    (ROGUE_COLUMNS * sizeof(dungeon[0][0])));
+			for (j = 0; j < ROGUE_COLUMNS; j++) {
+				write_uint16(fp, dungeon[i][j]);
+			}
 			for (j = 0; j < ROGUE_COLUMNS; j++) {
 				buf[j] = mvinch_rogue(i, j);
 			}
@@ -326,8 +614,9 @@ rw_dungeon(FILE *fp, bool rw)
 		return;
 	} else { // 読み込み時実行
 		for (i = 0; i < ROGUE_LINES; i++) {
-			r_read(fp, (char *)dungeon[i],
-			    (ROGUE_COLUMNS * sizeof(dungeon[0][0])));
+			for (j = 0; j < ROGUE_COLUMNS; j++) {
+				dungeon[i][j] = read_uint16(fp);
+			}
 			r_read(fp, buf, ROGUE_COLUMNS);
 
 			if (i < ROGUE_LINES - 1) {
@@ -359,15 +648,12 @@ rw_id(id id_table[], FILE *fp, int n, bool wr)
 
 	for (i = 0; i < n; i++) {
 		if (wr) {
-			r_write(fp, (char *)&(id_table[i].value),
-			    sizeof(short));
-			r_write(fp, (char *)&(id_table[i].id_status),
-			    sizeof(unsigned short));
+			write_int16(fp, id_table[i].value);
+			write_uint16(fp, id_table[i].id_status);
 			write_string(id_table[i].title, fp);
 		} else {
-			r_read(fp, (char *)&(id_table[i].value), sizeof(short));
-			r_read(fp, (char *)&(id_table[i].id_status),
-			    sizeof(unsigned short));
+			id_table[i].value = read_int16(fp);
+			id_table[i].id_status = read_uint16(fp);
 			read_string(id_table[i].title, fp);
 		}
 	}
@@ -384,7 +670,7 @@ write_string(char *s, FILE *fp)
 
 	n = strlen(s) + 1;
 	xxxx(s, n);
-	r_write(fp, (char *)&n, sizeof(short));
+	write_int16(fp, n);
 	r_write(fp, s, n);
 }
 
@@ -397,7 +683,7 @@ read_string(char *s, FILE *fp)
 {
 	short n;
 
-	r_read(fp, (char *)&n, sizeof(short));
+	n = read_int16(fp);
 	r_read(fp, s, n);
 	xxxx(s, n);
 }
@@ -412,36 +698,8 @@ rw_rooms(FILE *fp, bool rw)
 	short i;
 
 	for (i = 0; i < MAXROOMS; i++) {
-		rw ? r_write(fp, (char *)(rooms + i), sizeof(room))
-		   : r_read(fp, (char *)(rooms + i), sizeof(room));
-	}
-}
-
-/*
- * r_read
- * ファイルからバッファに読み込む
- */
-void
-r_read(FILE *fp, char *buf, int n)
-{
-	if (fread(buf, sizeof(char), n, fp) != n) {
-		clean_up("ファイルが読めません。");
-	}
-}
-
-/*
- * r_write
- * バッファの内容をファイルに書き込む
- */
-void
-r_write(FILE *fp, char *buf, int n)
-{
-	if (!write_failed) {
-		if (fwrite(buf, sizeof(char), n, fp) != n) {
-			message("ファイルに書けません。", 0);
-			sound_bell();
-			write_failed = 1;
-		}
+		rw ? write_room(fp, &rooms[i])
+		   : read_room(fp, &rooms[i]);
 	}
 }
 
@@ -474,33 +732,33 @@ has_been_touched(rogue_time *saved_time, rogue_time *mod_time)
 void
 write_obj(object *obj, FILE *fp)
 {
-	r_write(fp, (char *)&obj->m_flags, sizeof(obj->m_flags));
+	write_uint32(fp, obj->m_flags);
 
 	int32_t damage_size = utf8size(obj->damage);
-	r_write(fp, (char *)&damage_size, sizeof(damage_size));
+	write_int32(fp, damage_size);
 	r_write(fp, (char *)obj->damage, damage_size * sizeof(utf8_int8_t));
 
-	r_write(fp, (char *)&obj->quantity, sizeof(obj->quantity));
-	r_write(fp, (char *)&obj->ichar, sizeof(obj->ichar));
-	r_write(fp, (char *)&obj->kill_exp, sizeof(obj->kill_exp));
-	r_write(fp, (char *)&obj->is_protected, sizeof(obj->is_protected));
-	r_write(fp, (char *)&obj->is_cursed, sizeof(obj->is_cursed));
-	r_write(fp, (char *)&obj->class, sizeof(obj->class));
-	r_write(fp, (char *)&obj->identified, sizeof(obj->identified));
-	r_write(fp, (char *)&obj->which_kind, sizeof(obj->which_kind));
-	r_write(fp, (char *)&obj->o_row, sizeof(obj->o_row));
-	r_write(fp, (char *)&obj->o_col, sizeof(obj->o_col));
-	r_write(fp, (char *)&obj->o, sizeof(obj->o));
-	r_write(fp, (char *)&obj->row, sizeof(obj->row));
-	r_write(fp, (char *)&obj->col, sizeof(obj->col));
-	r_write(fp, (char *)&obj->d_enchant, sizeof(obj->d_enchant));
-	r_write(fp, (char *)&obj->quiver, sizeof(obj->quiver));
-	r_write(fp, (char *)&obj->trow, sizeof(obj->trow));
-	r_write(fp, (char *)&obj->tcol, sizeof(obj->tcol));
-	r_write(fp, (char *)&obj->hit_enchant, sizeof(obj->hit_enchant));
-	r_write(fp, (char *)&obj->what_is, sizeof(obj->what_is));
-	r_write(fp, (char *)&obj->picked_up, sizeof(obj->picked_up));
-	r_write(fp, (char *)&obj->in_use_flags, sizeof(obj->in_use_flags));
+	write_int16(fp, obj->quantity);
+	write_int8(fp, obj->ichar);
+	write_int16(fp, obj->kill_exp);
+	write_int8(fp, obj->is_protected);
+	write_int8(fp, obj->is_cursed);
+	write_int8(fp, obj->class);
+	write_int8(fp, obj->identified);
+	write_uint8(fp, obj->which_kind);
+	write_int8(fp, obj->o_row);
+	write_int8(fp, obj->o_col);
+	write_int8(fp, obj->o);
+	write_int8(fp, obj->row);
+	write_int8(fp, obj->col);
+	write_int8(fp, obj->d_enchant);
+	write_int8(fp, obj->quiver);
+	write_int8(fp, obj->trow);
+	write_int8(fp, obj->tcol);
+	write_int8(fp, obj->hit_enchant);
+	write_uint8(fp, obj->what_is);
+	write_int8(fp, obj->picked_up);
+	write_uint8(fp, obj->in_use_flags);
 }
 
 /*
@@ -510,33 +768,32 @@ write_obj(object *obj, FILE *fp)
 void
 read_obj(object *obj, FILE *fp)
 {
-	r_read(fp, (char *)&obj->m_flags, sizeof(obj->m_flags));
+	obj->m_flags = read_uint32(fp);
 
-	int32_t damage_size = 0;
-	r_read(fp, (char *)&damage_size, sizeof(damage_size));
+	int32_t damage_size = read_int32(fp);
 	obj->damage = malloc(damage_size * sizeof(utf8_int8_t));
 	r_read(fp, (char *)obj->damage, damage_size * sizeof(utf8_int8_t));
 
-	r_read(fp, (char *)&obj->quantity, sizeof(obj->quantity));
-	r_read(fp, (char *)&obj->ichar, sizeof(obj->ichar));
-	r_read(fp, (char *)&obj->kill_exp, sizeof(obj->kill_exp));
-	r_read(fp, (char *)&obj->is_protected, sizeof(obj->is_protected));
-	r_read(fp, (char *)&obj->is_cursed, sizeof(obj->is_cursed));
-	r_read(fp, (char *)&obj->class, sizeof(obj->class));
-	r_read(fp, (char *)&obj->identified, sizeof(obj->identified));
-	r_read(fp, (char *)&obj->which_kind, sizeof(obj->which_kind));
-	r_read(fp, (char *)&obj->o_row, sizeof(obj->o_row));
-	r_read(fp, (char *)&obj->o_col, sizeof(obj->o_col));
-	r_read(fp, (char *)&obj->o, sizeof(obj->o));
-	r_read(fp, (char *)&obj->row, sizeof(obj->row));
-	r_read(fp, (char *)&obj->col, sizeof(obj->col));
-	r_read(fp, (char *)&obj->d_enchant, sizeof(obj->d_enchant));
-	r_read(fp, (char *)&obj->quiver, sizeof(obj->quiver));
-	r_read(fp, (char *)&obj->trow, sizeof(obj->trow));
-	r_read(fp, (char *)&obj->tcol, sizeof(obj->tcol));
-	r_read(fp, (char *)&obj->hit_enchant, sizeof(obj->hit_enchant));
-	r_read(fp, (char *)&obj->what_is, sizeof(obj->what_is));
-	r_read(fp, (char *)&obj->picked_up, sizeof(obj->picked_up));
-	r_read(fp, (char *)&obj->in_use_flags, sizeof(obj->in_use_flags));
+	obj->quantity = read_int16(fp);
+	obj->ichar = read_int8(fp);
+	obj->kill_exp = read_int16(fp);
+	obj->is_protected = read_int8(fp);
+	obj->is_cursed = read_int8(fp);
+	obj->class = read_int8(fp);
+	obj->identified = read_int8(fp);
+	obj->which_kind = read_uint8(fp);
+	obj->o_row = read_int8(fp);
+	obj->o_col = read_int8(fp);
+	obj->o = read_int8(fp);
+	obj->row = read_int8(fp);
+	obj->col = read_int8(fp);
+	obj->d_enchant = read_int8(fp);
+	obj->quiver = read_int8(fp);
+	obj->trow = read_int8(fp);
+	obj->tcol = read_int8(fp);
+	obj->hit_enchant = read_int8(fp);
+	obj->what_is = read_uint8(fp);
+	obj->picked_up = read_int8(fp);
+	obj->in_use_flags = read_uint8(fp);
 	obj->next_object = NULL;
 }
